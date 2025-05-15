97acb815375ba90d3ec24b88ede0ceb7b151f777
Rick Meels <rick.meels@gmail.com>
2025-05-15 11:15:32 +0200

    test: added some unit/integration test

---
+++ IApiMarker.cs
@@ -0,0 +1,6 @@
+namespace _3d_printer_cost_calculator;
+
+public interface IApiMarker
+{
+    
+}
--- Models/ParsedGcode.cs
+++ Models/ParsedGcode.cs
@@ -2,13 +2,13 @@ namespace _3d_printer_cost_calculator.Models;
 
 public class ParsedGcode
 {
-    public double FilamentUsedMm { get; set; }
-    public double FilamentUsedGrams { get; set; }
+    public double? FilamentUsedMm { get; set; }
+    public double? FilamentUsedGrams { get; set; }
     public TimeSpan EstimatedPrintTime { get; set; }
-    public int LayerCount { get; set; }
-    public double LayerHeight { get; set; }
-    public int NozzleTemperature { get; set; }
-    public int BedTemperature { get; set; }
+    public int? LayerCount { get; set; }
+    public double? LayerHeight { get; set; }
+    public int? NozzleTemperature { get; set; }
+    public int? BedTemperature { get; set; }
     public string SlicerName { get; set; } = string.Empty;
     public string SlicerVersion { get; set; } = string.Empty;
 }
